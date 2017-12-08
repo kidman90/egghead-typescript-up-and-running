347a7f60d99a696ed30e644aca045367159017e8
kidman90 <kacprzak90@gmail.com>
2017-12-08 23:49:47 +0100

    03 Configuring TypeScript Which Files to Compile with "Files" and "OutDir"

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./two");
+var App = /** @class */ (function () {
+    function App() {
+    }
+    return App;
+}());
---
+++ dist/two.js
@@ -0,0 +1,6 @@
+"use strict";
+var Two = /** @class */ (function () {
+    function Two() {
+    }
+    return Two;
+}());
