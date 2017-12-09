7bb64a0b2077ceac0b42470a435ffac3f417ce6b
kidman90 <kacprzak90@gmail.com>
2017-12-09 20:53:37 +0100

    11 TypeScript - Understanding Decorators

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,16 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const _ = require("lodash");
-const Observable_1 = require("rxjs/Observable");
 require("rxjs/add/observable/timer");
 require("rxjs/add/observable/interval");
-Observable_1.Observable.interval(1000)
-    .subscribe(x => console.log(x));
+// Observable.interval(1000)
+//   .subscribe(x => console.log(x));
 class App {
     constructor() {
         this.title = "Eggheads";
@@ -15,3 +20,33 @@ class App {
     }
 }
 console.log(_.isArray(new App().getUsers()));
+// const person = { name: "John" };
+// function addAge(age: number) {
+//   return function (person: any) {
+//     return {
+//       age,
+//       name: person.name
+//     };
+//   };
+// }
+// const newPerson = addAge(30)(person);
+// console.log(newPerson);
+function addAge(age) {
+    return function (targetClass) {
+        return class {
+            constructor() {
+                this.name = new targetClass().name;
+                this.age = age;
+            }
+        };
+    };
+}
+let Person = class Person {
+    constructor() {
+        this.name = "John";
+    }
+};
+Person = __decorate([
+    addAge(20)
+], Person);
+console.log(new Person());
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
 import 'rxjs/add/observable/interval';
 
-Observable.interval(1000)
-  .subscribe(x => console.log(x));
+// Observable.interval(1000)
+//   .subscribe(x => console.log(x));
 
 class App implements SocialNetwork {
   title = "Eggheads";
@@ -16,3 +16,34 @@ class App implements SocialNetwork {
 }
 
 console.log(_.isArray(new App().getUsers()));
+
+// const person = { name: "John" };
+
+// function addAge(age: number) {
+//   return function (person: any) {
+//     return {
+//       age,
+//       name: person.name
+//     };
+//   };
+// }
+
+// const newPerson = addAge(30)(person);
+
+// console.log(newPerson);
+
+function addAge(age:number) {
+  return function (targetClass:any) {
+    return class {
+      name = new targetClass().name;
+      age = age;
+    };
+  };
+}
+
+@addAge(20)
+class Person {
+  name = "John";
+}
+
+console.log(new Person());
